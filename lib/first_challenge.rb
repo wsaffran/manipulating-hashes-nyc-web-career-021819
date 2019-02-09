875848bf1efa60221931d5508ae973f5f0e6fbae
Will Saffran <willh.saffran@gmail.com>
2019-02-09 19:41:11 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,8 +14,15 @@ def first_challenge
   }
 
   #your code here
-
-
+  
+  contacts.each do |person, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.delete_if {|flavor| flavor=="strawberry"}
+      end 
+    end 
+  end 
+   
   #remember to return your newly altered contacts hash!
   contacts
 end
