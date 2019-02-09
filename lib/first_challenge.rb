bcb2db20abdd37d808adb70f42c30f04f79c562d
Will Saffran <willh.saffran@gmail.com>
2019-02-09 19:20:37 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   #your code here
   contacts.each do |person, data|
     if person == :"Freddy Mercury"
-      data.each do |person, data|
+      data.each do |data, info|
         if attribute == :favorite_icecream_flavors
           :favorite_icecream_flavors.delete_if 
         
